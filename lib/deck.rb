81a673f0ac9aa3f763597bddc642cb6f41f9a390
MoonNoah <moon.503@buckeyemail.osu.edu>
2022-05-26 11:24:53 -0400

    removed shuffle method (unnessesary) and removed count variable, cleaned up other methods, added tests

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,46 +3,43 @@ require_relative 'card'
 
 class Deck
   # Created 5/15/2022 by Noah Moon
+  # Creates a populated and shuffled deck of 81 cards
+  # The deck is usable as its own object, but also returns a stack
   def initialize
-    @count = 0
-    @stack = reset_deck
+    @stack = reset
   end
 
   # Created 5/15/2022 by Noah Moon
-  def reset_deck
+  # Resets deck to its initial, populated and shuffled, state
+  def reset
     @stack = []
-    @count = 0
     lim = 1..3
-    for i in lim do
-      for j in lim do
-        for k in lim do
-          for l in lim do
-            @count += 1
-            @stack << Card.new(i,j,k,l)
+    for shade in lim do
+      for shape in lim do
+        for color in lim do
+          for count in lim do
+            @stack << Card.new(shade,shape,color,count)
           end
         end
       end
     end
-    return @stack
+    @stack = @stack.shuffle
   end
 
   # Created 5/15/2022 by Noah Moon
+  # Draws 1 card from the top of the deck
   def draw
-    @count -= 1
     @stack.pop
   end
 
   # Created 5/15/2022 by Noah Moon
+  # Gets current count of remaining cards
   def get_count
-    @count
+    @stack.count
   end
 
   # Created 5/15/2022 by Noah Moon
-  def shuffle
-    @stack = @stack.shuffle
-  end
-
-
+  # Prints the deck as an array with each cards description as its elements
   def to_s
     string_array = []
     for cards in @stack
@@ -50,4 +47,5 @@ class Deck
     end
     return string_array.to_s
   end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,32 +1,39 @@
+# File created 5/15/2022 by Noah Moon
 require_relative '../lib/deck'
+require_relative '../lib/card'
 
+# Created 5/15/2022 by Noah Moon
 describe 'Deck' do
   it 'creates a new deck' do
     deck = Deck.new
     expect(deck.get_count == 81).to be_truthy
   end
 
-  it 'draws cards' do
+  # Created 5/16/2022 by Noah Moon
+  it 'Returns cards when drawing' do
     deck = Deck.new
-    deck.draw
-    expect(deck.get_count == 80).to be_truthy
+    card = deck.draw
+    expect(card.class.to_s == "Card").to be_truthy
   end
 
-  it 'shuffles cards' do
+  # Created 5/15/2022 by Noah Moon
+  it 'resets to full deck from full' do
     deck = Deck.new
-    shuff_deck = Deck.new
-    shuff_deck.shuffle
-    expect(deck.to_s != shuff_deck.to_s).to be_truthy
+    deck.reset
+    expect(deck.get_count == 81).to be_truthy
   end
 
-  it 'starts a new deck' do
+  # Created 5/16/2022 by Noah Moon
+  it 'resets to full deck from depleated' do
     deck = Deck.new
-    deck.shuffle
-    new_deck = Deck.new
-    deck.reset_deck
-    expect(deck.to_s == new_deck.to_s).to be_truthy
+    while deck.get_count > 10 do
+      deck.draw
+    end
+    deck.reset
+    expect(deck.get_count == 81).to be_truthy
   end
 
+  # Created 5/15/2022 by Noah Moon
   it 'keeps count' do
     deck = Deck.new
     i = 50
@@ -35,6 +42,12 @@ describe 'Deck' do
       i -= 1
     end
     expect(deck.get_count == 31).to be_truthy
+  end
+
+  # Created 5/16/2022 by Noah Moon
+  it 'converts to string' do
+    deck = Deck.new
+    expect(deck.to_s.class == "".class).to be_truthy
   end
 
 end
