0e9c23b7d63459cb94da5a7320cbde7d66a60938
Daniel Wu <danielwu77777@gmail.com>
2022-05-26 18:49:46 -0400

    Small edits to game manager

--- GameManager.rb
+++ GameManager.rb
@@ -2,40 +2,31 @@
 class GameManager   
   
     # Created 5/25/2022 by Daniel Wu
+    # Edited 5/26/2022 by Daniel Wu: Deleted player scores since player class will handle
     # Initializes game
     def startGame
         # Get number of players
         puts "Enter number of players(2 maximum): "
         numPlayers = gets.to_i
-        # Create players
-        createPlayer numPlayers
+        if numPlayers != 1 && numPlayers != 2
+            puts "Invalid number of players. Expected 1 or 2 but got " + numPlayers.to_s
+        end
         # Create instance variables to determine who's turn it is (1 means playerOne's turn, 2 means playerTwo's turn)
         @playerTurn = 1;
-        # Initiate deck and dealer
-        deck.initialize
+        # Initiate dealer
         dealer.initialize
     end   
      
     # Created 5/25/2022 by Daniel Wu
-    # Creates instance variables for each player's score
-    def createPlayer(numPlayers)   
-        if numPlayers == 1
-            @playerOneScore = 0
-        elsif numPlayers == 2
-            @playerOneScore = 0
-            @playerTwoScore = 0
-        else 
-            puts "Invalid number of players. Expected 1 or 2 but got " + numPlayers.to_s
-        end
-    end   
-     
-    # Created 5/25/2022 by Daniel Wu
+    # Edited 5/26/2022 by Daniel Wu: Added call for player to choose cards
     # Switch turn for players
-    def switchTurn   
+    def switchTurn  
         if playerTurn == 1
             playerTurn = 2
+            chosenCardsArray = player.chooseCards
         else
             playerTurn = 1
+            chosenCardsArray = player.chooseCards
         end
     end      
 
