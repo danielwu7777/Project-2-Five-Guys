a9050b3ac0880c91c94e99ce112fedf3cb2b7434
MoonNoah <moon.503@buckeyemail.osu.edu>
2022-05-28 16:12:28 -0400

    added header for card validator

---
+++ lib/hand_validator.rb
@@ -0,0 +1,10 @@
+# File created 5/28/2022 by Noah Moon
+class Hand_Validate
+
+  # Created 5/28/2022 by Noah Moon
+  # cards: set of cards that the user selected
+  def self.validate_hand(cards)
+
+  end
+end
+
