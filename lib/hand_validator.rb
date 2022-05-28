7b3092032b5d3a76ac8eef5902e97ce4beaa22d4
MoonNoah <moon.503@buckeyemail.osu.edu>
2022-05-28 17:50:19 -0400

    Added hand validator implementation and testing

--- lib/hand_validator.rb
+++ lib/hand_validator.rb
@@ -1,9 +1,21 @@
+require_relative '../lib/card'
 # File created 5/28/2022 by Noah Moon
 class Hand_Validate
 
   # Created 5/28/2022 by Noah Moon
   # cards: set of cards that the user selected
+  # returns true if there is some attribute that is the same for all cards, ot there is no common attribute
   def self.validate_hand(cards)
+    different = true
+    attribue_array = cards.map{|card| card.get_attribute_array}
+    card1 = attribue_array[0]
+    card2 = attribue_array[1]
+    card3 = attribue_array[2]
+    card1.each_index { |index|
+      return true if card1[index] == card2[index] and card2[index] == card3[index]
+      different = different && card1[index] != card2[index] && card2[index] != card3[index]
+    }
+    return different
 
   end
 end
---
+++ spec/hand_validator_spec.rb
@@ -0,0 +1,97 @@
+# File created 5/15/2022 by Noah Moon
+require_relative '../lib/deck'
+require_relative '../lib/card'
+require_relative '../lib/hand_validator'
+require 'set'
+
+# Created 5/15/2022 by Noah Moon
+describe 'Hand_Validator' do
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when same first attribute is same' do
+    card_set = Set.new
+    card_set.add(Card.new 1,1,1,1)
+    card_set.add(Card.new 1,2,3,4)
+    card_set.add(Card.new 1,3,3,3)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when same second attribute is same' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,2,1,1)
+    card_set.add(Card.new 2,2,2,2)
+    card_set.add(Card.new 3,2,3,3)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when same third attribute is same' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,1,3,1)
+    card_set.add(Card.new 2,2,3,2)
+    card_set.add(Card.new 3,3,3,3)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when same fourth attribute is same' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,1,1,4)
+    card_set.add(Card.new 2,2,2,4)
+    card_set.add(Card.new 3,3,3,4)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when many attributes are same' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,2,1,1)
+    card_set.add(Card.new 1,2,2,2)
+    card_set.add(Card.new 1,2,3,3)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when all attributes are same' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,2,3,4)
+    card_set.add(Card.new 1,2,3,4)
+    card_set.add(Card.new 1,2,3,4)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns false when 1 attribute is different' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,1,1,1)
+    card_set.add(Card.new 2,2,2,2)
+    card_set.add(Card.new 2,3,3,3)
+
+    expect(Hand_Validate.validate_hand card_set).to be_falsey
+  end
+
+  # Created 5/28/2022 by Noah Moon
+  it 'returns true when all attributes are different' do
+    deck = Deck.new
+    card_set = Set.new
+    card_set.add(Card.new 1,1,1,1)
+    card_set.add(Card.new 2,2,2,2)
+    card_set.add(Card.new 3,3,3,3)
+
+    expect(Hand_Validate.validate_hand card_set).to be_truthy
+  end
+
+end
