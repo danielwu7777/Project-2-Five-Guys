b955a0dd5d61bd04b2e087218be7018b5241f140
danielwu3412 <wu.3412@buckeyemail.osu.edu>
2022-05-28 16:49:47 -0400

    Deleted test_game_manager.rb and edited demo.rb

--- demo.rb
+++ demo.rb
@@ -1,6 +1,6 @@
 #File created by Jake McCann 5/27/2022
+# Edited by Daniel Wu 5/28/2022: Delete call to startGame and added call to endGame
 require_relative 'lib/game_manager'
 
 test_manager = GameManager.new
-
-test_manager.startGame
+test_manager.endGame
--- lib/test_game_manager.rb
+++
@@ -1,6 +0,0 @@
-# File created 5/26/2022 by Daniel Wu 
-# Edited 5/27/2022 by Daniel Wu: deleted call to game.startGame
-require_relative '../lib/game_manager'
-
-game = GameManager.new
-game.endGame
