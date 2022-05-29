7c1f52df4be9dc6503c10cb5f02606f953a202c9
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-05-29 12:47:28 -0400

    nested enums, removed translation methods from Card.rb

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,14 @@
 require_relative 'attribute_enums'
 
 # File created 5/24/2022 by Jake McCann
-# Edited 5/25/2022 by Jake McCann
+# Edited 5/25/2022 by Jake McCann: added attribute translation methods
+# Edited 5/29/2022 by Jake McCann: removed attribute translation methods
 class Card
 
   # Created 5/24/2022 by Jake McCann
+  # Edited 5/29/2022 by Jake McCann: Changed order of attribute array so to_s makes grammatical sense
   def initialize(shading, shape, color, count)
-    @attribute_array = [shading, shape, color, count]
+    @attribute_array = [count, shading, color, shape]
   end
 
   # Created 5/24/2022 by Jake McCann
@@ -14,29 +16,12 @@ class Card
     return @attribute_array
   end
 
-  # Created 5/25/2022 by Jake McCann
-  def shading_translation
-    SHADING_TYPE.key(@attribute_array[0]).to_s
-  end
-
-  # Created 5/25/2022 by Jake McCann
-  def shape_translation
-    SHAPE_TYPE.key(@attribute_array[1]).to_s
-  end
-
-  # Created 5/25/2022 by Jake McCann
-  def color_translation
-    COLOR_TYPE.key(@attribute_array[2]).to_s
-  end
-
-  # Created 5/25/2022 by Jake McCann
-  def count_translation
-    COUNT_TYPE.key(@attribute_array[3]).to_s
-  end
-
   # Created 5/24/2022 by Jake McCann
   # Edited 5/25/2022 by Jake McCann: Used translation methods to make more concise
+  # Edited 5/29/2022 by Jake McCann: Use nested dictionary to remove translation methods
   def to_s
-    "#{count_translation} #{shading_translation} #{color_translation} #{shape_translation}"
+    @attributes_as_string = Array. new
+    @attribute_array.each_index { |index| @attributes_as_string << ATTRIBUTES_ENUM[index].key(@attribute_array[index]).to_s }
+    @attributes_as_string.join " "
   end
 end
