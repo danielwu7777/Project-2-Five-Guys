9c62ca27477b871190d37880c52499973e3bdef5
MoonNoah <moon.503@buckeyemail.osu.edu>
2022-05-31 15:30:19 -0400

    fixed remove_3_cards method, added more tests, removed unnessesary methods

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -25,43 +25,37 @@ class Dealer
            deal_card
         end
         Game_Table.display_cards(@dealt_cards)
-    end   
-    
-    # Edited 5/25/2022 by Mingxuan Gong  
-    # pick the specified card
-    def get_card(i)
-        @dealt_cards[i]
     end
-    
-    # Edited 5/25/2022 by Mingxuan Gong
+
+    # Created 5/25/2022 by Mingxuan Gong
     # Edited 5/31/2022 by Noah Moon
     # adds 3 new cards to dealt cards
     def add_three_cards
-        3.times {@dealt_cards.push(@deck.draw)}
-    end
-    
-    # Edited 5/25/2022 by Mingxuan Gong
-    # add a card from the deck and show it on the table 
-    def deal_card
-            
-        card = @deck.draw
-        @dealt_cards.push(card)
-        
+        3.times {deal_card}
     end
 
     # Created 5/31/2022 by Noah Moon
     # Removes 3 specified cards from chosen cards
     # index_set: set of card indexes to be removed
     def remove_3_cards(index_set)
-        index_set.each { |index| delete_card(index)  }
+        card_array = Array.new
+        index_set.each { |index| card_array.push(@dealt_cards[index])  }
+        card_array.each { |card| delete_card(card)  }
     end
 
     private
+
     # Edited 5/25/2022 by Mingxuan Gong
-    # Edited 5/31/2022 by Noah Moon
     # remove one card from the table   
     def delete_card(card)
-        @dealt_cards.delete_at(card)
+        @dealt_cards.delete(card)
+    end
+
+    # Edited 5/25/2022 by Mingxuan Gong
+    # add a card from the deck and show it on the table
+    def deal_card
+        card = @deck.draw
+        @dealt_cards.push(card)
     end
 
 end
--- spec/dealer_spec.rb
+++ spec/dealer_spec.rb
@@ -1,7 +1,8 @@
-# File created 5/26/2022 by Daniel Wu
+# File created 5/31/2022 by Noah Moon
 require_relative '../lib/dealer'
+require_relative '../lib/game_table'
 
-# Created 5/26/2022 by Daniel Wu
+# Created 5/31/2022 by Noah Moon
 describe 'Dealer' do
 
   # Created 5/31/2022 by Noah Moon
@@ -28,6 +29,29 @@ describe 'Dealer' do
     dealer.start_game
     dealer.remove_3_cards index_set
     expect(dealer.dealt_cards.length == 9 && dealer.deck.get_count == 81-12).to be_truthy
+  end
+
+  # Created 5/31/2022 by Noah Moon
+  it 'removes 3 cards from and adds 3 cards to dealt cards' do
+    dealer = Dealer.new
+    index_set = Set[1,2,3]
+    dealer.start_game
+    dealer.remove_3_cards index_set
+    dealer.add_three_cards
+
+    expect(dealer.dealt_cards.length == 12 && dealer.deck.get_count == 81-15).to be_truthy
+  end
+
+  # Created 5/31/2022 by Noah Moon
+  it 'removes correct cards' do
+    dealer = Dealer.new
+    index_set = Set[1,2,3]
+    dealer.start_game
+    cards = [dealer.dealt_cards[0], dealer.dealt_cards[1], dealer.dealt_cards[2] ]
+    dealer.remove_3_cards index_set
+    dealer.add_three_cards
+
+    expect(dealer.dealt_cards.include?(cards[1]) || dealer.dealt_cards.include?(cards[2]) || dealer.dealt_cards.include?(cards[3])).to be_falsey
   end
 
 
