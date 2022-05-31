c4597f81b5aabe61f33f81fd4c967e9a736d2dff
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-05-31 14:25:48 -0400

    finished integration tests between dealer and deck

--- spec/integration_dealer_deck_spec.rb
+++ spec/integration_dealer_deck_spec.rb
@@ -17,4 +17,12 @@ describe 'Dealer and Deck' do
     test_dealer.deal_card
     expect(test_dealer.deck.get_count == pre_deal_deck_count - 1).to be_truthy
   end
+
+  # Create 5/31/2022 by Jake McCann
+  it 'removes 3 cards from deck when dealer deals three cards' do
+    test_dealer = Dealer.new
+    pre_deal_deck_count = test_dealer.deck.get_count
+    test_dealer.add_three_cards
+    expect(test_dealer.deck.get_count == pre_deal_deck_count - 3).to be_truthy
+  end
 end
