c7e456599ca7e58d0caae85b3955020da35dbd61
Jake McCann <mccann.307@buckeyemail.osu.edu>
2022-05-31 14:18:25 -0400

    rename spec, initial integration tests

--- spec/integration_card_deck_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative '../lib/dealer'
-
-describe 'Dealer and Deck' do
-  it 'removes 12 cards from deck when dealer created' do
-
-  end
-end
---
+++ spec/integration_dealer_deck_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/dealer'
+require_relative '../lib/deck'
+
+# File created 5/31/2022 by Jake McCann
+describe 'Dealer and Deck' do
+  # Created 5/31/2022 by Jake McCann
+  it 'removes 12 cards from deck when dealer created' do
+    test_deck = Deck.new
+    test_dealer = Dealer.new
+    expect(test_dealer.deck.get_count == test_deck.get_count - 12).to be_truthy
+  end
+
+  # Create 5/31/2022 by Jake McCann
+  it 'removes 1 card from deck when dealer deals a card' do
+    test_dealer = Dealer.new
+    pre_deal_deck_count = test_dealer.deck.get_count
+    test_dealer.deal_card
+    expect(test_dealer.deck.get_count == pre_deal_deck_count - 1).to be_truthy
+  end
+end
