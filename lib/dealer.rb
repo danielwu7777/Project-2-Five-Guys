b74888f616fb9c1aac71276e23b043dce3cc445a
danielwu3412 <wu3412@buckeyemail.osu.edu>
2022-05-31 23:22:32 -0400

    merge changes from dealer_replace into implement_hand_validator

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -4,7 +4,11 @@ require_relative 'game_table'
 
 # File created 5/24/2022 by Mingxuan Gong
 # Edited 5/25/2022 by Mingxuan Gong
+# Edited 5/31/2022 by Noah Moon
 class Dealer
+    # Created 5/31/2022 by Noah Moon
+    attr_reader :dealt_cards, :deck
+
     # generate a table to show 12 cards
     # Edited 5/27/2022 by Jake McCann: fixed capitaliztion on deck
     def initialize
@@ -16,43 +20,42 @@ class Dealer
     # Create 5/27/2022 by Mingxuan Gong
     # Edited 5/25/2022 by Mingxuan Gong
     # lay out 12 cards on the table    
-    def start_game() 
+    def start_game
         while @dealt_cards.length < 12 do
            deal_card
         end
         Game_Table.display_cards(@dealt_cards)
-    end   
-    
-    # Edited 5/25/2022 by Mingxuan Gong  
-    # pick the specified card
-    def get_card(i)
-        @table[i]
     end
-    
-    # Edited 5/25/2022 by Mingxuan Gong
-    # if there aren't a set in the table then add 3 more cards to the table
-    def add_three_cards()
 
+    # Created 5/25/2022 by Mingxuan Gong
+    # Edited 5/31/2022 by Noah Moon
+    # adds 3 new cards to dealt cards
+    def add_three_cards
+        3.times {deal_card}
     end
-    
-    # Edited 5/25/2022 by Mingxuan Gong
-    # add a card from the deck and show it on the table 
-    def deal_card()
-            
-        card = @deck.draw
-        @dealt_cards.push(card)
-        
-    end 
-    
+
+    # Created 5/31/2022 by Noah Moon
+    # Removes 3 specified cards from chosen cards
+    # index_set: set of card indexes to be removed
+    def remove_3_cards(index_set)
+        card_array = Array.new
+        index_set.each { |index| card_array.push(@dealt_cards[index])  }
+        card_array.each { |card| delete_card(card)  }
+    end
+
+    private
+
     # Edited 5/25/2022 by Mingxuan Gong
     # remove one card from the table   
     def delete_card(card)
         @dealt_cards.delete(card)
     end
-    
+
     # Edited 5/25/2022 by Mingxuan Gong
-    # return the length of the card
-    def length
-    	@table.length()
+    # add a card from the deck and show it on the table
+    def deal_card
+        card = @deck.draw
+        @dealt_cards.push(card)
     end
+
 end
--- lib/hand_validator.rb
+++ lib/hand_validator.rb
@@ -7,8 +7,13 @@ class Hand_Validate
   # Edited 5/29/2022 by Noah Moon
   # Edited 5/31/2022 by Daniel Wu: Fixed typos in comments and reformatted methods
   # cards: set of cards that the user selected
+<<<<<<< HEAD
   # returns true if there is some attribute that is the same for all cards, or there is no common attribute
   def self.validate_hand(cards)
+=======
+  # returns true if there is some attribute that is the same for all cards, ot there is no common attribute
+  def self.validate_hand?(cards)
+>>>>>>> origin/dealer_replace
     valid = true
     attribute_array = cards.map{|card| card.get_attribute_array}
 
@@ -18,6 +23,7 @@ class Hand_Validate
     valid
   end
 
+  private
   # Created 5/30/2022 by Noah Moon
   def self.is_same?(attribute_array, index)
     attribute_array[0][index] == attribute_array[1][index] && attribute_array[1][index] == attribute_array[2][index]
