88fe74ee3dcc808f92140471510bb009d36596f8
MoonNoah <moon.503@buckeyemail.osu.edu>
2022-06-01 16:29:39 -0400

    removed unnessesary methods, made methods more terse, fixed tests

--- lib/dealer.rb
+++ lib/dealer.rb
@@ -5,33 +5,26 @@ require_relative 'game_table'
 # File created 5/24/2022 by Mingxuan Gong
 # Edited 5/25/2022 by Mingxuan Gong
 # Edited 5/31/2022 by Noah Moon
+# Edited 6/1/2022 by Noah Moon
 class Dealer
     # Created 5/31/2022 by Noah Moon
     attr_reader :dealt_cards, :deck
 
     # generate a table to show 12 cards
     # Edited 5/27/2022 by Jake McCann: fixed capitaliztion on deck
+    # Edited 6/1/2022 by Noah Moon
     def initialize
-        @dealt_cards = Array.new
-        @deck = Deck.new
+        @dealt_cards, @deck = Array.new, Deck.new
         start_game
-    end
-    
-    # Create 5/27/2022 by Mingxuan Gong
-    # Edited 5/25/2022 by Mingxuan Gong
-    # lay out 12 cards on the table    
-    def start_game
-        while @dealt_cards.length < 12 do
-           deal_card
-        end
-        Game_Table.display_cards(@dealt_cards)
     end
 
     # Created 5/25/2022 by Mingxuan Gong
     # Edited 5/31/2022 by Noah Moon
+    # Edited 6/1/2022 by Noah Moon
     # adds 3 new cards to dealt cards
     def add_three_cards
-        3.times {deal_card}
+        3.times {@dealt_cards.push(@deck.draw)}
+        Game_Table.display_cards(@dealt_cards)
     end
 
     # Created 5/31/2022 by Noah Moon
@@ -40,22 +33,16 @@ class Dealer
     def remove_3_cards(index_set)
         card_array = Array.new
         index_set.each { |index| card_array.push(@dealt_cards[index])  }
-        card_array.each { |card| delete_card(card)  }
+        card_array.each { |card| @dealt_cards.delete(card)  }
     end
 
     private
-
+    # Create 5/27/2022 by Mingxuan Gong
     # Edited 5/25/2022 by Mingxuan Gong
-    # remove one card from the table   
-    def delete_card(card)
-        @dealt_cards.delete(card)
+    # Edited 6/1/2022 by Noah Moon
+    # lay out 12 cards on the table
+    def start_game
+        12.times{@dealt_cards.push(@deck.draw)}
+        Game_Table.display_cards(@dealt_cards)
     end
-
-    # Edited 5/25/2022 by Mingxuan Gong
-    # add a card from the deck and show it on the table
-    def deal_card
-        card = @deck.draw
-        @dealt_cards.push(card)
-    end
-
 end
